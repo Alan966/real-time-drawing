fd75bfa46b58f69034ca4b10901339028c9af7c2
Alan Jair Guadalupe Marinez <66544589+Alan966@users.noreply.github.com>
2021-11-12 18:19:09 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ require(`./socket`)(io)
 
 app.set(`port`, process.env.PORT || 5000)
 app.use(express.static(path.join(__dirname, `public`)))
-app.use(cars())
+app.use(cors())
 
 server.listen(app.get(`port`), () => {
     console.log(`Aplicacion corriendo el el puerto: ${app.get(`port`)}`)
